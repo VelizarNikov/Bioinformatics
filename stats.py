13856fdb44fd0a9b29a2e9e1146dd39abe18c227
VelizarNikov <nikovvelizar@gmail.com>
2025-11-04 18:33:02 +0000

    D: added reverse complement function to stats.py

--- stats.py
+++ stats.py
@@ -2,3 +2,12 @@ import collections
 
 def base_count(genome):
      return collections.Counter(genome)
+
+dna = "AGCTCATGCT" #example seq
+
+def reverse_complement(seq):
+     complement_dict = {"A": "T", "T": "A", "G": "C", "C": "G"}
+     complement_dna = ""
+     for base in seq[::-1]:
+          complement_dna += complement_dict[base]
+     return complement_dna
